240b87cda57202ad146eb500f0ea21d6804b8c49
RafiaBonna <rafiaakterbonna@gmail.com>
2025-11-12 23:48:23 +0600

    my

--- scm-project/resources/views/pages/raw_material_purchase/index.blade.php
+++ scm-project/resources/views/pages/raw_material_purchase/index.blade.php
@@ -81,7 +81,7 @@
                                 </tr>
                             @empty
                                 <tr>
-                                    <td colspan="7" class="text-center">No Suppliers found.</td>
+                                    <td colspan="7" class="text-center">No Suppliers Found.</td>
                                 </tr>
                             @endforelse
                         </tbody>
