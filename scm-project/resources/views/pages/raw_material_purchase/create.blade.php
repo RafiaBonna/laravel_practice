7c9b509e4411c8242cc61f65d19156a907b83470
RafiaBonna <rafiaakterbonna@gmail.com>
2025-11-14 23:52:58 +0600

    my

--- scm-project/resources/views/pages/raw_material_purchase/create.blade.php
+++ scm-project/resources/views/pages/raw_material_purchase/create.blade.php
@@ -13,28 +13,5 @@
     @endif
     
     {{-- Display ALL validation errors at the top for visibility --}}
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form action="{{ route('superadmin.raw-material-purchases.store') }}" method="POST">
-        @csrf
-
-        {{-- INVOICE MASTER DATA --}}
-        <div class="row mb-3">
-            <div class="col-md-4">
-                <label>Invoice Number</label>
-                {{-- Use old() to retain value --}}
-                <input type="text" name="invoice_number" class="form-control" required value="{{ old('invoice_number') }}">
-                @error('invoice_number') <small class="text-danger">{{ $message }}</small> @enderror
-            </div>
-            <div class="col-md-4">
-                <label>Invoice Date</label>
-                <input type="date" name="invoice_date" class="form-control" required value="{{ old('invoice_date', date('Y-m-d')) }}">
+_date" class="form-control" required value="{{ old('invoice_date', date('Y-m-d')) }}">
 
