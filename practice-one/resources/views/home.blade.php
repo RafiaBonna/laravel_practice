4c188ce5c2eed58ca266042083c926b5e8942fc7
RafiaBonna <rafiaakterbonna@gmail.com>
2025-11-25 23:33:48 +0600

    my

--- practice-one/resources/views/home.blade.php
+++ practice-one/resources/views/home.blade.php
@@ -9,8 +9,6 @@
     <h2>This is my home page</h2>
     <h3>This is new 3</h3>
     <h4>This is new 4</h4>
-    <h6>This is new 4</h6>
-    <h7>This is new 4</h7>
-    <h5>This is new 4</h5>
+    
 </body>
 </html>
